c3d2f963c2832d9598f24011a0c61f1a6abfe926
elya <vavina.elvira@gmail.com>
2023-03-30 21:00:32 +0300

    Generic relationships

--- codio/blog/admin.py
+++ codio/blog/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 admin.site.register(Tag)
-
+# admin.site.register(Comment)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ['title', 'author', 'summary', 'created_at', 'modified_at']
-    # fields = [field.name for field in Post._meta.get_fields()]
+    # fields = [field.name for field in Post._meta.get_fields()] #error created_at (and modified_at) is not editable
+
 
 
 
 admin.site.register(Post, PostAdmin)
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-# 	list_display = ('slug',)
-# 	prepopulated_fields = {"slug": ("title",)}
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Comment._meta.get_fields() if field.name not in ('id', 'creator')]
+	# exclude = ["creator"]
